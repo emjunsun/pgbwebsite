c3b69ccf402201878be3ae5697badd3d7e68a6cc
Jun Sun <em.junsun@gmail.com>
2016-02-22 18:36:13 +0800

    测试<title>标签

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def title(site_title)
+    content_for(:site_title, site_title.to_s.html_safe) # html_safe because it's escaped again later (yield?)
+    site_title
+  end
 end
